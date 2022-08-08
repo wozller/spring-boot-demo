1852df8238bcedc1f9c07e462ccb99be306efcf7
wozller <brian.sanchez.wozller@gmail.com>
2022-08-08 13:31:38 -0400

    Make DB ArrayList final

--- src/main/java/com/wozller/springbootdemo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/wozller/springbootdemo/dao/FakePersonDataAccessService.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 @Repository("fakeDao")
 public class FakePersonDataAccessService implements PersonDao {
 
-    private static List<Person> DB = new ArrayList<>();
+    private static final List<Person> DB = new ArrayList<>();
 
     @Override
     public int insertPerson(UUID id, Person person) {
