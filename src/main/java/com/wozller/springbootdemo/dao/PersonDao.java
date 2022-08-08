9b259b04e2da1b8a08cb375a5721b7e0ef2f37e0
wozller <brian.sanchez.wozller@gmail.com>
2022-08-08 15:52:42 -0400

    Add functionality to get all people in the server
    This means creating a method that retrieves the database ArrayList and
    serves a GET request (HTTP).

--- src/main/java/com/wozller/springbootdemo/dao/PersonDao.java
+++ src/main/java/com/wozller/springbootdemo/dao/PersonDao.java
@@ -2,6 +2,7 @@ package com.wozller.springbootdemo.dao;
 
 import com.wozller.springbootdemo.model.Person;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PersonDao {
@@ -13,4 +14,6 @@ public interface PersonDao {
         UUID id = UUID.randomUUID();
         return insertPerson(id, person);
     }
+
+    List<Person> selectAllPeople();
 }
