decdbd3e5d58a8395239f870013a0b029642658a
wozller <brian.sanchez.wozller@gmail.com>
2022-08-08 14:49:41 -0400

    Fix path for @RequestMapping annotation

--- src/main/java/com/wozller/springbootdemo/api/PersonController.java
+++ src/main/java/com/wozller/springbootdemo/api/PersonController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("localhost:8080/api/v1/person")
+@RequestMapping("api/v1/person")
 @RestController
 public class PersonController {
 
