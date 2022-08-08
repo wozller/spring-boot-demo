2ce0b158db1061edeea5f105798ce5ce90508f21
wozller <brian.sanchez.wozller@gmail.com>
2022-08-08 13:29:57 -0400

    Add @Qualifier annotation so finish autowire to fakeDao

--- src/main/java/com/wozller/springbootdemo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/wozller/springbootdemo/dao/FakePersonDataAccessService.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.UUID;
 
-@Repository
+@Repository("fakeDao")
 public class FakePersonDataAccessService implements PersonDao {
 
     private static List<Person> DB = new ArrayList<>();
--- src/main/java/com/wozller/springbootdemo/service/PersonService.java
+++ src/main/java/com/wozller/springbootdemo/service/PersonService.java
@@ -3,6 +3,7 @@ package com.wozller.springbootdemo.service;
 import com.wozller.springbootdemo.model.Person;
 import com.wozller.springbootdemo.dao.PersonDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +12,7 @@ public class PersonService {
     private final PersonDao personDao;
 
     @Autowired
-    public PersonService(PersonDao personDao) {
+    public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
         this.personDao = personDao;
     }
 
