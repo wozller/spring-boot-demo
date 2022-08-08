ebd48c4f1098e516e9565c238422a42bb931bbce
wozller <brian.sanchez.wozller@gmail.com>
2022-08-08 12:03:19 -0400

    Create "fake person" data access class to implement PersonDao
    Since we created the PersonDao interface to give us the ability to use
    any implementation of a database we want, we will implement said
    interface in a new class called FakePersonDataAccessService. This will
    allow us to work with an ArrayList that can simulate our database and
    the insertPerson method.

---
+++ src/main/java/com/wozller/springbootdemo/dao/FakePersonDataAccessService.java
@@ -0,0 +1,18 @@
+package com.wozller.springbootdemo.dao;
+
+import com.wozller.springbootdemo.model.Person;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class FakePersonDataAccessService implements PersonDao {
+
+    private static List<Person> DB = new ArrayList<>();
+
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        DB.add(new Person(id, person.getName()));
+        return 1;
+    }
+}
