0ca7979dcf276c5cf3780bfb5cab54e0987ea787
wozller <brian.sanchez.wozller@gmail.com>
2022-08-08 13:27:25 -0400

    Add annotation to autowire into interface

--- src/main/java/com/wozller/springbootdemo/service/PersonService.java
+++ src/main/java/com/wozller/springbootdemo/service/PersonService.java
@@ -2,6 +2,7 @@ package com.wozller.springbootdemo.service;
 
 import com.wozller.springbootdemo.model.Person;
 import com.wozller.springbootdemo.dao.PersonDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,6 +10,7 @@ public class PersonService {
 
     private final PersonDao personDao;
 
+    @Autowired
     public PersonService(PersonDao personDao) {
         this.personDao = personDao;
     }
