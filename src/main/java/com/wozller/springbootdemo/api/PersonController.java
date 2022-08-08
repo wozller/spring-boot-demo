a5c514555b7652cb50a28d3282706cff459d49d2
wozller <brian.sanchez.wozller@gmail.com>
2022-08-08 13:36:50 -0400

    Add @RestController annotation to PersonController
    PersonController is not available as a RestController so we need to add
    the @RestController annotation. We can then have some methods, and then
    expose some endpoints that clients can consume.
    
    Going back to our architecture, we can note that we would be receiving
    HTTP requests (GET, POST, DELETE, and PUT). For whatever client we have
    (React, Android, etc.) to issue requests to our backend, we need to make
    the class available as  RestController.

--- src/main/java/com/wozller/springbootdemo/api/PersonController.java
+++ src/main/java/com/wozller/springbootdemo/api/PersonController.java
@@ -3,7 +3,9 @@ package com.wozller.springbootdemo.api;
 import com.wozller.springbootdemo.model.Person;
 import com.wozller.springbootdemo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class PersonController {
 
     private final PersonService personService;
