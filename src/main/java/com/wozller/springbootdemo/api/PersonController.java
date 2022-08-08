ab77f7bccb2ced60835b6cb7895b034e63617da9
wozller <brian.sanchez.wozller@gmail.com>
2022-08-08 13:33:39 -0400

    Autowire to PersonController (API Layer)

--- src/main/java/com/wozller/springbootdemo/api/PersonController.java
+++ src/main/java/com/wozller/springbootdemo/api/PersonController.java
@@ -2,11 +2,13 @@ package com.wozller.springbootdemo.api;
 
 import com.wozller.springbootdemo.model.Person;
 import com.wozller.springbootdemo.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class PersonController {
 
     private final PersonService personService;
 
+    @Autowired
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
